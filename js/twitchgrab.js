f4c9e54e52a73d7ec083a24cabf92d5425710de5
Keith Murphy <keith@murfbard.com>
2016-10-23 20:42:15 -0400

    fixed indenting

--- js/twitchgrab.js
+++ js/twitchgrab.js
@@ -24,11 +24,10 @@ function pullTwitchData() {
       // sorted by number of current viewers on Twitch, 
       // most popular first.
       // km,10.16
-	  
+    
       var counter = 0;
-	  
-      _.each(twitchGamesObject.top, function(eachGame) {
-		
+    
+      _.each(twitchGamesObject.top, function(eachGame) {    
         $.ajax({
           type: 'GET',
           url: 'https://api.twitch.tv/kraken/streams?game=' + eachGame.game.name,
@@ -38,21 +37,21 @@ function pullTwitchData() {
           success: function(twitchStreamsObject) {
             // Twitch returns a list of stream objects (each 
             // accessible by .streams[i]) sorted by number of 
-            // viewers descending. -- km,10.16		
+            // viewers descending. -- km,10.16    
             streams.push( new StreamData( eachGame.game.name, twitchStreamsObject.streams));
-		      }
-		    });
-		    counter++;
-	    });  
-	  }
+          }
+        });
+        counter++;
+      });  
+    }
   })
 }
 
 $('input').keypress(function(e) {
   if( isKeypressANumber(e)) {
-	waitingWriteTable();
+    waitingWriteTable();
   } else {
-	e.preventDefault(); //stop character from entering input
+    e.preventDefault(); //stop character from entering input
   }
 });
 
@@ -61,7 +60,7 @@ var waitingWriteTable = _.debounce(writeTable, 1000);
 
 function writeTable() {
   // TODO: look this over closely, this is likely not finished
-	
+  
   var viewers = $('input').val()
   var tr;
   var yourRank;
@@ -69,36 +68,36 @@ function writeTable() {
   removeTableRows("tablebyviewers");
   
   for(iStream=0; iStream<100; iStream++){
-	if(streams[iStream] && streams[iStream].Channel && (streams[iStream].Channel.length > 0 )){
-      var foundStreamerRanking = false;	  
-      for(iChannel=0; !foundStreamerRanking && iChannel<10; iChannel++){
-        if(streams[iStream].Channel && streams[iStream].Channel[iChannel] && (streams[iStream].Channel[iChannel].viewers < viewers)){
-          yourRank = iChannel + 1;
-		  foundStreamerRanking = true;
+    if(streams[iStream] && streams[iStream].Channel && (streams[iStream].Channel.length > 0 )){
+        var foundStreamerRanking = false;   
+        for(iChannel=0; !foundStreamerRanking && iChannel<10; iChannel++){
+          if(streams[iStream].Channel && streams[iStream].Channel[iChannel] && (streams[iStream].Channel[iChannel].viewers < viewers)){
+            yourRank = iChannel + 1;
+            foundStreamerRanking = true;
+          }
         }
-      }
-	
-      if (!foundStreamerRanking) {
-        yourRank = "Very Low";
-      }
-  	
-      if (yourRank < 5) {
-        tr = $('<tr class="success"/>');
-      } else if (yourRank < 15 || yourRank < 3) {
-        tr = $('<tr/>');
-      } else {
-        tr = $('<tr class="danger"/>');
-      }
-  
-      tr.append("<td>" + streams[iStream].GameName + "</td>");
-      tr.append("<td>" + yourRank + "</td>");
-	  tr.append("<td>" + (streams[iStream].Channel.length > 0 ? streams[iStream].Channel[0].viewers : "No Streamer") + "</td>");
-	  tr.append("<td>" + (streams[iStream].Channel.length > 2 ? streams[iStream].Channel[2].viewers : "No Streamer") + "</td>");
-	  tr.append("<td>" + (streams[iStream].Channel.length > 4 ? streams[iStream].Channel[4].viewers : "No Streamer") + "</td>");
-	  tr.append("<td>" + (streams[iStream].Channel.length > 9 ? streams[iStream].Channel[9].viewers : "No Streamer") + "</td>");
-      $('.tablebyviewers').append(tr);
-      console.log("we reach here.");	
-	}
+    
+        if (!foundStreamerRanking) {
+          yourRank = "Very Low";
+        }
+      
+        if (yourRank < 5) {
+          tr = $('<tr class="success"/>');
+        } else if (yourRank < 15 || yourRank < 3) {
+          tr = $('<tr/>');
+        } else {
+          tr = $('<tr class="danger"/>');
+        }
+    
+        tr.append("<td>" + streams[iStream].GameName + "</td>");
+        tr.append("<td>" + yourRank + "</td>");
+        tr.append("<td>" + (streams[iStream].Channel.length > 0 ? streams[iStream].Channel[0].viewers : "No Streamer") + "</td>");
+        tr.append("<td>" + (streams[iStream].Channel.length > 2 ? streams[iStream].Channel[2].viewers : "No Streamer") + "</td>");
+        tr.append("<td>" + (streams[iStream].Channel.length > 4 ? streams[iStream].Channel[4].viewers : "No Streamer") + "</td>");
+        tr.append("<td>" + (streams[iStream].Channel.length > 9 ? streams[iStream].Channel[9].viewers : "No Streamer") + "</td>");
+        $('.tablebyviewers').append(tr);
+        console.log("we reach here.");  
+    }
   }
   sortTable("tablebyviewers");  
 }
